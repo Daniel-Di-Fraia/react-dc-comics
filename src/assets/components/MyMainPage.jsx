8d16ebb37a9dd3ad758f2d7b14886e19e5affb40
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-24 15:15:19 +0200

    finalizzazione header

---
+++ src/assets/components/MyMainPage.jsx
@@ -0,0 +1,12 @@
+// creo il main della pagina e lo esporto
+// const MyMainPage = () => {
+//     return (
+//         <main>
+//             <section>
+
+//             </section>
+//         </main>>
+//     )
+// }
+
+// export default MyMainPage
