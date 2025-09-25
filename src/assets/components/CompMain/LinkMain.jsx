df93fc32a223e668368e4c0fa06dc8328f4bb6d0
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-25 13:16:09 +0200

    divisione sezioni main content

---
+++ src/assets/components/CompMain/LinkMain.jsx
@@ -0,0 +1,50 @@
+const LinkMain = () => {
+    return (
+        <nav>
+                <ul className="nav-main">
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="/buy-comics-digital-comics.png" alt="Logo" />
+                            </figure>
+                            <span>DIGITAL COMICS</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="/buy-comics-merchandise.png" alt="Logo" />
+                            </figure>
+                            <span>DC MERCHANDISE</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="/buy-comics-subscriptions.png" alt="Logo" />
+                            </figure>
+                            <span>SUBSCRIPTION</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="/buy-comics-shop-locator.png" alt="Logo" />
+                            </figure>
+                            <span>COMIC SHOP LOCATOR</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="/buy-dc-power-visa.svg" alt="Logo" />
+                            </figure>
+                            <span>DC POWER VISA</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+    )
+}
+
+export default LinkMain
--- src/assets/components/MyMainPage.jsx
+++ src/assets/components/MyMainPage.jsx
@@ -1,3 +1,4 @@
+import LinkMain from "./CompMain/LinkMain"
 // creo il main della pagina e lo esporto
 const MyMainPage = () => {
     return (
@@ -10,50 +11,7 @@ const MyMainPage = () => {
 
             {/* sezione links sito */}
             <section className="site-nav">
-                <nav>
-                <ul className="nav-main">
-                    <li>
-                        <a href="#">
-                            <figure>
-                                <img src="/buy-comics-digital-comics.png" alt="Logo" />
-                            </figure>
-                            <span>DIGITAL COMICS</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <figure>
-                                <img src="/buy-comics-merchandise.png" alt="Logo" />
-                            </figure>
-                            <span>DC MERCHANDISE</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <figure>
-                                <img src="/buy-comics-subscriptions.png" alt="Logo" />
-                            </figure>
-                            <span>SUBSCRIPTION</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <figure>
-                                <img src="/buy-comics-shop-locator.png" alt="Logo" />
-                            </figure>
-                            <span>COMIC SHOP LOCATOR</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <figure>
-                                <img src="/buy-dc-power-visa.svg" alt="Logo" />
-                            </figure>
-                            <span>DC POWER VISA</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+               <LinkMain/>
             </section>
         </main>
     )
